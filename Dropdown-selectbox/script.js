8b9c3f29bc78f76d65768b4fdad3ae168ac5ab21
sooyyoung <sooyyyoung@gmail.com>
2022-05-12 17:47:45 +0900

    버튼 토글 js 추가

---
+++ Dropdown-selectbox/script.js
@@ -0,0 +1,6 @@
+const selectBtn = document.querySelector('.select-btn');
+const listBox = document.querySelector('.list-language');
+
+selectBtn.addEventListener('click', () => {
+    selectBtn.classList.toggle('on');
+})
