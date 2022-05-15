c56e20687c19634e374395ed7197b23a2ddaa0b1
sooyyoung <sooyyyoung@gmail.com>
2022-05-15 16:34:21 +0900

    선택한 언어 버튼에 반영

--- Dropdown-selectbox/script.js
+++ Dropdown-selectbox/script.js
@@ -1,6 +1,16 @@
 const selectBtn = document.querySelector('.select-btn');
 const listBox = document.querySelector('.list-language');
+const listBtn = document.querySelectorAll('.listBtn');
 
+// 버튼 토글
 selectBtn.addEventListener('click', () => {
     selectBtn.classList.toggle('on');
+})
+
+// 선택한 언어 버튼에 반영
+listBtn.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const btnName = e.currentTarget.textContent;
+        selectBtn.textContent = btnName;
+    })
 })
