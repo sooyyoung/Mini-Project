641696265e1518d082d425c9b21589cf5b934577
sooyyoung <sooyyyoung@gmail.com>
2022-05-15 17:13:47 +0900

    버튼 blur 추가

--- Dropdown-selectbox/script.js
+++ Dropdown-selectbox/script.js
@@ -3,14 +3,20 @@ const listBox = document.querySelector('.list-language');
 const listBtn = document.querySelectorAll('.listBtn');
 
 // 버튼 토글
-selectBtn.addEventListener('click', () => {
+selectBtn.addEventListener('mousedown', () => {
     selectBtn.classList.toggle('on');
-})
+});
+
+// 버튼 블러
+selectBtn.addEventListener('blur', () => {
+    selectBtn.classList.remove('on');
+});
 
 // 선택한 언어 버튼에 반영
 listBtn.forEach(item => {
-    item.addEventListener('click', (e) => {
+    item.addEventListener('mousedown', (e) => {
         const btnName = e.currentTarget.textContent;
         selectBtn.textContent = btnName;
-    })
-})
+    });
+    
+});
