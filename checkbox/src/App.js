929672c3b6dfc5291023b125944d6d026c37720f
sooyoung <sooyyyoung@gmail.com>
2022-10-18 16:05:15 +0900

    체크박스 마크업

---
+++ checkbox/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Checkbox from "./Checkbox";
+
+function App() {
+  return (
+    <div className="App">
+      <Checkbox />
+    </div>
+  );
+}
+
+export default App;
---
+++ checkbox/src/Checkbox.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Checkbox = () => {
+  const formData = [
+    { id: 1, name: "딸기🍓" },
+    { id: 2, name: "바나나🍌" },
+    { id: 3, name: "포도🍇" },
+    { id: 4, name: "오렌지🍊" },
+    { id: 5, name: "레몬🍋" },
+    { id: 6, name: "복숭아🍑" },
+    { id: 7, name: "파인애플🍍" },
+    { id: 8, name: "키위🥝" },
+    { id: 9, name: "사과🍎" },
+  ];
+
+  return (
+    <div className="checkbox">
+      <div className="allCheck">
+        <label>
+          <input type="checkbox" />
+          <p>전체 선택</p>
+        </label>
+      </div>
+      {formData.map((item) => {
+        return (
+          <div className="check">
+            <label key={item.id}>
+              <input type="checkbox" value={item.name} />
+              <p>{item.name}</p>
+            </label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Checkbox;
